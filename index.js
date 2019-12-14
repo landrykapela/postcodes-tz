aaa45e19310129c268acfd17c3f4dd34c70d77d9
Landry Kapela <landrykapela@gmail.com>
2019-12-14 15:12:45 +0300

    Add method to get city names in sorted manner

--- index.js
+++ index.js
@@ -5,20 +5,14 @@ exports.getPostcode = name => {
   let result = source.regions.filter(r => {
     if (name !== undefined) {
       //remove hyphens: possible in dar-es-salaam
-      // name.replace("-", " ");
+      name.replace("-", " ");
       return r.name.toLowerCase() == name.toLowerCase();
     } else {
       throw (TypeError, "The method getPostcode() expects a name parameter");
     }
   });
-  if (result.length > 0) {
-    let postcode = result[0].postcode;
-    // console.log("result: ", postcode);
-    return postcode;
-  } else {
-    // console.log("result: ", name + " is not a valid region/city in Tanzania");
-    return "Unknown";
-  }
+  if (result.length > 0) return result[0].postcode;
+  else return "Unknown";
 };
 
 //get all list of postcodes with name
@@ -27,4 +21,19 @@ exports.getPostcodes = () => {
   source.regions.map(r => {
     result.push({ name: r.name, postcode: r.postcode });
   });
+  return result;
+};
+
+//get all orted city/region names only
+exports.getCityNames = sortOrder => {
+  let result = [];
+  source.regions.map(r => {
+    result.push(r.name);
+  });
+
+  if (sortOrder === undefined) return result;
+
+  if (sortOrder.toLowerCase() === "asc") return result.sort();
+
+  if (sortOrder.toLowerCase() === "desc") return result.sort().reverse();
 };
