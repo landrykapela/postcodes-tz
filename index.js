9e16c3d4e9f76692792249bf6b8105e5c2c104c5
Landry Kapela <landrykapela@gmail.com>
2019-12-14 11:54:46 +0300

    Create main index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const source = require("./source.json");
+
+//get postcode by using name
+exports.getPostcode = name => {
+  let result = source.regions.filter(r => {
+    if (name !== undefined) {
+      //remove hyphens: possible in dar-es-salaam
+      // name.replace("-", " ");
+      return r.name.toLowerCase() == name.toLowerCase();
+    } else {
+      throw new Error("The method getPostcode() expects a name parameter");
+    }
+  });
+  if (result.length > 0) {
+    let postcode = result[0].postcode;
+    console.log("result: ", postcode);
+    return postcode;
+  } else {
+    console.log("result: ", name + " is not a valid region/city in Tanzania");
+    return "N/A";
+  }
+};
+
+//get all list of postcodes with name
+exports.getPostcodes = () => {
+  let result = [];
+  source.regions.map(r => {
+    result.push({ name: r.name, postcode: r.postcode });
+  });
+};
