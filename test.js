9319baf1d898f5fad732e4c99c4d0638c542ff7c
Landry Kapela <landrykapela@gmail.com>
2019-12-14 13:15:59 +0300

    Add Travis CI

--- test.js
+++ test.js
@@ -1,36 +1,4 @@
 const postcode = require("./index");
-// const chai = require("chai");
-// const jest = require("jest");
-// let expect = chai.expect;
-
-// describe("#getPostcode", () => {
-//   //test with a wrong city/region name parameter
-//   context("with incorrect city name paramater", () => {
-//     it("should return false", () => {
-//       expect(() => {
-//         postcode.getPostcode("New York");
-//       }).to.equal(false);
-//     });
-//   });
-
-//   //test without a name parameter
-//   context("without a name parameter", () => {
-//     it("should throw error", () => {
-//       expect(() => {
-//         postcode.getPostcode();
-//       }).to.throw(Error, "The method getPostcode() expects a name parameter");
-//     });
-//   });
-
-//   //test with a correct city/region name parameter
-//   context("with a correct region/city name parameter", () => {
-//     it("should return number equal to postcode", () => {
-//       expect(() => {
-//         postcode.getPostcode("Dar es salaam");
-//       }).to.equal(11000);
-//     });
-//   });
-// });
 
 //test with correct city name
 test("find postcode of Dar es Salaam", () => {
